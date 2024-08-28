a1cd4e8071fb4bcedd0a977ed2ff0a609df9d148
Sebastian Zuluaga Sosa <127877540+szuluaga644@users.noreply.github.com>
2024-08-27 23:31:36 -0500

    Se agrega Main 2 con paso de informacion

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -1,6 +1,11 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    TextView txvNombre, txvEdad;
+    Button salir;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        txvNombre = findViewById(R.id.textoNombre);
+        txvEdad = findViewById(R.id.textoEdad);
+        salir = findViewById(R.id.btnSalir);
+
+        Intent intent = getIntent();
+        String nombreUsuario = intent.getStringExtra("nombreUsuario");
+        String edadUsuario = intent.getStringExtra("edadUsuario");
+
+        txvNombre.setText(nombreUsuario);
+        txvEdad.setText(edadUsuario);
+
+        salir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
         });
     }
 }
